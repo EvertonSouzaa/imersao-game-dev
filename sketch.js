d397af00c768fd3bdbaf630ef43592dc9948c41c
Everton Souza <evertonryrotsu@hotmail.com>
2020-06-23 17:21:25 -0300

    Aula 02 Fluxo da vida  missao e falha

--- sketch.js
+++ sketch.js
@@ -1,30 +1,119 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let cenario;
 let somDoJogo;
+let somDoPulo;
 let personagem;
+let inimigo;
+
+
+const matrizPersonagem = [
+[0, 0],
+[220, 0],
+[440, 0],
+[660, 0],
+[0, 270],
+[220, 270],
+[440, 270],
+[660, 270],
+[0, 540],
+[220, 540],
+[440, 540],
+[660, 540],
+[0, 810],
+[220, 810],
+[440, 810],
+[660, 810],
+
+]
+
+const matrizInimigo = [
+  [0, 0],
+  [105, 0],
+  [210, 0],
+  [315, 0],
+  [0, 104],
+  [105, 104],
+  [210, 104],
+  [315, 104],
+  [0, 208],
+  [105, 208],
+  [210, 208],
+  [315, 208],
+  [0, 312],
+  [105, 312],
+  [210, 312],
+  [315, 312],
+  [0, 409],
+  [105, 409],
+  [210, 409],
+  [315, 409],
+  [0, 503],
+  [105, 503],
+  [210, 503],
+  [315, 503],
+  [0, 609],
+  [105, 609],
+  [210, 609],
+  [315, 609],
+]
 
 function preload() {
-  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemCenario = loadImage('imagens/cenario/floresta1.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somDoPulo = loadSound('sons/jump.mp3');
+  
+
+ 
 }
 
 function setup() {
+
+  // let tamanhoInimigo = width - 52;
+  // let larguraInimigo = 52;
+  // let alturaInimigo = 52;
+  // let larguraSprite = 104;
+  // let alturaSprite = 104;
+
+  // variavel global para pegar a largura e altura da tela
   createCanvas(windowWidth, windowHeight);
+  // velocidade do cenario
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(imagemPersonagem);
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52,
+    52, 104, 104);
+  // inimigo = new Inimigo(matrizInimigo, imagemInimigo, tamanhoInimigo, larguraInimigo, 
+  //   alturaInimigo, larguraSprite, alturaSprite);
   frameRate(40);
-  somDoJogo.loop();
-  
+
+  // somDoJogo.loop();
 }
 
-function draw() { 
+function keyPressed() {
+  if( key === 'ArrowUp') {
+    personagem.pula();
+    somDoPulo.play();
+  }
+}
+
+// funçao para desenhar, visualizar alguma animação na tela
+function draw() {
   cenario.exibe();
   cenario.move();
 
   personagem.exibe();
+  personagem.aplicaGravidade();
   
+  inimigo.exibe();
+  inimigo.move();
   
-}
+  if(personagem.estaColidindo(inimigo)) {
+    window.alert("GAME OVER")
+    console.log('colidiu')
+    noLoop();
+  }
 
+}
